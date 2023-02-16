c3ee26e4263babb4a0a91c3c5fc3804cef7f330a
jonem3 <matt.phil.jones@gmail.com>
2023-02-16 15:55:01 +0000

    Arm Updates

--- src/arm/arm.h
+++ src/arm/arm.h
@@ -6,12 +6,15 @@ private:
     float L1 = 102;
     float L2 = 79;
     float L3 = 136; // NEEDS TO BE UPDATED WHEN PEN END EFFECTOR ADDED
+
     double S1, S2, S3;
+
     ServoMotor Base = ServoMotor(4, 1488, 1.571, -1.571);
     ServoMotor Shoulder = ServoMotor(3, 1488, 1.571, -1.571);
     ServoMotor Wrist = ServoMotor(2, 1481, 1.571, -1.571);
-    void getAngles(int x, int y);  
+
+    void getAngles(int x, int y);
 
 public:
-void setArmPosition(int x, int y);
+    void setArmPosition(int x, int y);
 };
