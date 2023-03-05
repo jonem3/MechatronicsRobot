0fbb932d8f9a0679184c4b88f932f1c22a29d34f
mattp <matt.phil.jones@gmail.com>
2023-03-05 16:59:32 +0000

    Updated Motor Class

--- src/motors/motor.h
+++ src/motors/motor.h
@@ -1,7 +1,26 @@
 
-    public:
-        motor(int speed, bool ai1, bool ai2, bool standby);
-        void stop();
-        void reverse();
-        void adjustspeed(int pwm);
+#include <Arduino.h>
+class motor
+{
+private:
+    int pinI1;
+    int pinI2;
+    int pinPWM;
+    int pinStandBy;
+
+    bool I1;
+    bool I2;
+    bool standBy;
+    bool isMirrored;
+
+    unsigned char pwmValue;
+
+    int velocityLimiter(int velocity);
+    void setDirection(int velocity);
+
+public:
+    motor(int I1, int I2, int PWM, int StandBy, bool mirror);
+    void moveMotor(int velocity);
+    void motorStandBy();
+    void brake();
 };
