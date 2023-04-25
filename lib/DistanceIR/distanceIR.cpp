beea208b4ec5b2fa05b2c7df903ff34b38239130
Aryamanchandak <aryamanchandak@gmail.com>
2023-04-25 11:08:23 +0100

    DistanceiR - final

--- lib/DistanceIR/distanceIR.cpp
+++ lib/DistanceIR/distanceIR.cpp
@@ -2,12 +2,12 @@
 
 Distanceir :: Distanceir (int pin) 
 {
-    analogPin = pin ; 
+    Analogpin = pin ; 
 }
 
-int Distanceir :: FindDistance (float irDistance)
+int Distanceir :: FindDistance ()
 {
-    // Why input irDistance? Just read the ir sensor here like: int irDistance = analogRead(analogPin);
+    int irDistance = analogRead(Analogpin);
     distance = 0.0001*(irDistance*irDistance) - 0.1439*(irDistance) + 49.9653 ;// replace x as irDistance 
     return distance ;
 }
